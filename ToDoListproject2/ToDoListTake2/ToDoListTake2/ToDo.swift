2437038baf3c568ccdef2fbc83233494b28d08ed
helenhall <68516050+helenhall@users.noreply.github.com>
2021-07-19 12:08:55 -0400

    Add files via upload

---
+++ ToDoListproject2/ToDoListTake2/ToDoListTake2/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListTake2
+//
+//  Created by Helen Hall on 7/19/21.
+//
+
+import UIKit
+
+class ToDoClass {
+    
+    var description = ""
+    var important = false
+}
