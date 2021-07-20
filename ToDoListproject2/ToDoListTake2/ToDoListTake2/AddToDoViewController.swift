3e35660b36d1607db64bf484eeaaaae0b9ce97d3
helenhall <68516050+helenhall@users.noreply.github.com>
2021-07-20 10:42:15 -0400

    Add files via upload

--- ToDoListproject2/ToDoListTake2/ToDoListTake2/AddToDoViewController.swift
+++ ToDoListproject2/ToDoListTake2/ToDoListTake2/AddToDoViewController.swift
@@ -20,16 +20,28 @@ class AddToDoViewController: UIViewController {
     }
     
     @IBAction func addButtonTapped(_ sender: UIButton) {
-        let newToDo = ToDoClass()
-        
-        if let checkForInput = descriptionInput.text {
-            newToDo.description = checkForInput
-            newToDo.important = switchInput.isOn
-            
+        guard let accessToCoreData = UIApplication.shared.delegate as? AppDelegate else {
+            return
         }
         
-        previousToDoTVC.listOfToDo.append(newToDo)
-        previousToDoTVC.tableView.reloadData()
+        let dataFromCoreData = accessToCoreData.persistentContainer.viewContext
+    
+        
+        let newToDo = ToDoCD(context: dataFromCoreData)
+        
+        newToDo.descriptionInCD = descriptionInput.text
+        newToDo.importantInCD = switchInput.isOn
+        
+        accessToCoreData.saveContext()
+        
+//        if let checkForInput = descriptionInput.text {
+//            newToDo.description = checkForInput
+//            newToDo.important = switchInput.isOn
+//
+//        }
+//
+//        previousToDoTVC.listOfToDo.append(newToDo)
+//        previousToDoTVC.tableView.reloadData()
         navigationController?.popViewController(animated: true)
     }
     
--- ToDoListproject2/ToDoListTake2/ToDoListTake2/ToDoTableViewController.swift
+++ ToDoListproject2/ToDoListTake2/ToDoListTake2/ToDoTableViewController.swift
@@ -9,27 +9,36 @@ import UIKit
 
 class ToDoTableViewController: UITableViewController {
     
-    var listOfToDo : [ToDoClass] = []
+    var listOfToDo : [ToDoCD] = []
+//
+//
+//    func createToDo () -> [ToDoClass] {
+//
+//        let swiftToDo = ToDoClass()
+//        swiftToDo.description = "Learn Swift"
+//        swiftToDo.important = true
+//
+//        let dogToDo = ToDoClass()
+//        dogToDo.description = "Walk the Dog"
+//
+//        return [swiftToDo, dogToDo]
+//
+//    }
     
-    
-    func createToDo () -> [ToDoClass] {
-        
-        let swiftToDo = ToDoClass()
-        swiftToDo.description = "Learn Swift"
-        swiftToDo.important = true
-        
-        let dogToDo = ToDoClass()
-        dogToDo.description = "Walk the Dog"
-        
-        return [swiftToDo, dogToDo]
-        
+    func getToDos() {
+        if let accessToCoreData = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            if let dataFromCoreData = try? accessToCoreData.fetch(ToDoCD.fetchRequest()) as? [ToDoCD] {
+                listOfToDo = dataFromCoreData
+                tableView.reloadData()
+            }
+        }
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        listOfToDo = createToDo()
+//        listOfToDo = createToDo()
     }
 
     // MARK: - Table view data source
@@ -51,20 +60,33 @@ class ToDoTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        
         let eachToDo = listOfToDo[indexPath.row]
         
-        if eachToDo.important {
-            cell.textLabel?.text = "!"  + eachToDo.description
-        } else {
-        cell.textLabel?.text = eachToDo.description
+        if let thereIsDescription = eachToDo.descriptionInCD {
+            if eachToDo.importantInCD {
+                cell.textLabel?.text = "!" + thereIsDescription
+            } else {
+                cell.textLabel?.text = eachToDo.descriptionInCD
+            }
+           
         }
-        // Configure the cell...
-
+//
+//        let eachToDo = listOfToDo[indexPath.row]
+//
+//        if eachToDo.importantInCD {
+//            cell.textLabel?.text = "!"  + eachToDo.descriptionInCD!
+//        } else {
+//        cell.textLabel?.text = eachToDo.descriptionInCD
+//        }
+//        // Configure the cell...
+//
         return cell
     }
     
 
+    override func viewWillAppear(_ animated: Bool) {
+        getToDos()
+    }
     
 
 
@@ -76,21 +98,25 @@ class ToDoTableViewController: UITableViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+  
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        if let nextAddToDoVC = segue.destination as? AddToDoViewController {
-            nextAddToDoVC.previousToDoTVC = self
-        }
-        
+    if let nextAddToDoVC = segue.destination as? AddToDoViewController{
+        nextAddToDoVC.previousToDoTVC = self
+    }
+  
+     
         if let nextCompletedToDoVC = segue.destination as? CompletedToDoViewController {
-            if let chosenToDo = sender as? ToDoClass {
+if let chosenToDo = sender as? ToDoCD {
                 nextCompletedToDoVC.selectedToDo = chosenToDo
                 nextCompletedToDoVC.previousToDoTVC = self
-            }
+//            }
         }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
+}
+}
     
 
-}
+
